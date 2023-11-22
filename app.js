40cc453aa791c51f1149da581ef3f771d5d73256
PEDRO HENRIQUE INOCENTE <23111824@CFP127ALUNO.LOCAL>
2023-11-22 12:26:19 -0300

    1push

---
+++ app.js
@@ -0,0 +1,12 @@
+/************************************************************************
+ * Objetivo: Criação de uma API para uma pizzaria
+ * Data: 22/11/2023
+ * Autor: Pedro Henrique Inocente
+ * Versão: 1.0
+*************************************************************************/
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
